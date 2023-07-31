221a5a99fa66f8f280a79c84f37e69e954fce824
Souheil HADJ HABIB <souheil.hadjhabib@groupe-hasnaoui.com>
2023-07-31 09:56:05 +0100

    finished chapter 24

---
+++ app/src/main/java/com/example/draganddraw/Box.kt
@@ -0,0 +1,18 @@
+package com.example.draganddraw
+
+import android.graphics.PointF
+
+class Box(val start:PointF) {
+    var end:PointF = start
+    val left: Float
+        get() = start.x.coerceAtMost(end.x)
+
+    val right: Float
+        get() = start.x.coerceAtLeast(end.x)
+
+    val top: Float
+        get() = start.y.coerceAtMost(end.y)
+
+    val bottom: Float
+        get() = start.y.coerceAtLeast(end.y)
+}
